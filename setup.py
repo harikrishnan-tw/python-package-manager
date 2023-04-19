cf18634c8005c7d2c80a8d1b3c4f6d3f5328abd4
harikrishnan-tw <86928653+harikrishnan-tw@users.noreply.github.com>
2023-04-20 00:28:24 +0530

    configured versioneer

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
+import versioneer
+
 setup(
     name='haris_first_package',
-    version='1.0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description='A sample python package to learn how to maintain private python packages',
     author='Hari Krishnan',
     author_email='hariraghupathy@gmail.com',
     package_dir={
-    'package_1': 'my_package_1',
-    'package_2': 'my_package_2'
+    'package_1': 'src/my_package_1',
+    'package_2': 'src/my_package_2'
     },
     packages=['package_1', 'package_2']
 )
